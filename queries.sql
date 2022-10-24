0b87cb35ee0c4aca80d0960db4cd524f5e0dd834
Ibnballo1 <belloabdullateef035@gmail.com>
2022-10-24 22:03:00 +0100

    Update queries

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name LIKE '%mon';
-SELECT name from Animal22 WHERE date_of_birth BETWEEN '2016%' AND '2019%'; -- Yet to work
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-02-12' AND '2019-05-07';
 SELECT name from animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth from animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
