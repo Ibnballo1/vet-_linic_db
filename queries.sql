3f8343501834a95e8cf0e00fc7bcf61d3188b075
Ibnballo1 <belloabdullateef035@gmail.com>
2022-10-26 14:22:03 +0100

    Query database with transactions and aggregate functions

--- queries.sql
+++ queries.sql
@@ -8,3 +8,67 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = true;
 SELECT * from Animal22 WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Transactions
+begin;
+update animals
+set species = 'unspecified';
+select species from animals;
+rollback;
+select species from animals;
+
+begin;
+update animals
+set species = 'digimon'
+where name like '%mon';
+
+begin;
+update animals
+set species = 'pokemon'
+where name not like '%mon';
+
+select * from animals;
+
+begin;
+delete from animals;
+rollback;
+select * from animals;
+
+begin;
+delete from animals where date_of_birth > '2022-01-01';
+savepoint my_savepoint;
+update animals
+set weight_kg = weight_kg * -1;
+rollback to my_savepoint;
+update animals
+set weight_kg = weight_kg * -1;
+where weight_kg < 0;
+commit;
+
+-- AGGREGATE FUNCTIONS AND GROUP BY
+-- How many animals are there?
+select count(name)
+from animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(escape_attempts)
+FROM animals
+WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT name, MAX(escape_attempts) -- yet to solve
+FROM animals;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT
+MIN(weight_kg), MAX(weight_kg)
+FROM animals;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT
+AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01';
