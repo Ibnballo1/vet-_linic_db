145bfe5a5cf82928595e988b962bf94e3aef41bb
Ibnballo1 <belloabdullateef035@gmail.com>
2022-10-28 08:38:26 +0100

    Add more tables to database

--- schema.sql
+++ schema.sql
@@ -13,3 +13,28 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD species varchar(100);
+
+ create table owners (
+  id int not null,
+  full_name varchar(250),
+  age int,
+  primary key (id)
+);
+
+create table species (
+  id int not null,
+  name varchar(250),
+  primary key (id)
+);
+
+ALTER TABLE animals
+ADD primary key(id);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD species_id INT REFERENCES species(id);
+
+ALTER TABLE animals
+ADD owner_id INT REFERENCES owners(id);
